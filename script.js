33148995b6dc2e064e7f9729899ecd2362207ed5
joshua rubin <joshua@thisroom.org>
2024-01-04 15:54:15 -0800

    random

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function playGlissando() {
+function playRandomGlissando() {
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     let startTime = audioContext.currentTime;
-    let endTime = startTime + 0.5 * 8; // Duration of the entire glissando
+    let endTime = startTime + 4; // Duration of the glissando
+
+    const startFrequency = randomFrequency(65.41, 2093); // Random frequency between C2 and C7
+    const endFrequency = randomFrequency(65.41, 2093); // Another random frequency between C2 and C7
 
     const oscillator = audioContext.createOscillator();
-    oscillator.frequency.setValueAtTime(261.63, startTime); // Start at C4
-    oscillator.frequency.linearRampToValueAtTime(523.25, endTime); // End at C5
+    oscillator.frequency.setValueAtTime(startFrequency, startTime);
+    oscillator.frequency.linearRampToValueAtTime(endFrequency, endTime);
     oscillator.connect(audioContext.destination);
     oscillator.start(startTime);
     oscillator.stop(endTime);
 }
 
+function randomFrequency(min, max) {
+    return Math.random() * (max - min) + min;
+}
